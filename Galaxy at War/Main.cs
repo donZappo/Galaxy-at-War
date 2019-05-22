637cc4c14f8e36d7cc2d6f391646fa47f33781b7
Bradley Burcar <btburcar@gmail.com>
2019-05-22 18:20:21 -0400

    Added some stuff

--- Galaxy at War/Main.cs
+++ Galaxy at War/Main.cs
@@ -23,6 +23,60 @@ namespace Galaxy_at_War
             Settings = Helper.ModSettings.ReadSettings(settings);
         }
 
+        public static void RefreshResources(SimGameState Sim)
+        {
+            try
+            {
+                foreach (KeyValuePair<Faction, FactionDef> pair in Sim.FactionsDict)
+                {
+                    if (!IsExcluded(pair.Key))
+                    {
+                        if (Fields.factionResources.Find(x => x.faction == pair.Key) == null)
+                        {
+                            Fields.factionResources.Add(new FactionResources(pair.Key, 0, 0));
+                        }
+                        else
+                        {
+                            FactionResources resources = Fields.factionResources.Find(x => x.faction == pair.Key);
+                            resources.offence = 0;
+                            resources.defence = 0;
+                        }
+                    }
+                }
+                if (Sim.Starmap != null)
+                {
+                    foreach (StarSystem system in Sim.StarSystems)
+                    {
+                        FactionResources resources = Fields.factionResources.Find(x => x.faction == system.Owner);
+                        if (resources != null)
+                        {
+                            if (!IsExcluded(resources.faction))
+                            {
+                                resources.offence += Mathf.RoundToInt(GetOffenceValue(system) * (1 - (Fields.WarFatique[system.Owner] / 100)));
+                                resources.defence += Mathf.RoundToInt(GetDefenceValue(system) * (1 - (Fields.WarFatique[system.Owner] / 100)));
+                            }
+                        }
+                    }
+                }
+                if (Fields.settings.debug)
+                {
+                    foreach (KeyValuePair<Faction, FactionDef> pair in Sim.FactionsDict)
+                    {
+                        if (!IsExcluded(pair.Key))
+                        {
+                            FactionResources resources = Fields.factionResources.Find(x => x.faction == pair.Key);
+                            Logger.LogMonthlyReport(Helper.GetFactionName(pair.Key, Sim.DataManager) + " Exhaustion:" + Fields.WarFatique[pair.Key] + " Attack:" + resources.offence + " Defence:" + resources.defence);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+            }
+        }
+
+
         //Find how many friendly and opposing neighbors are present for the star system.
         public class TargetSystem
         {
