afae39910b068641853fd82778b403d23bbda9c0
gnivler <gnivil@gmail.com>
2019-05-26 16:45:13 -0600

    reworked, possibly still horrible

--- Galaxy at War/RelationTracker.cs
+++ Galaxy at War/RelationTracker.cs
@@ -1,5 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using BattleTech;
+using Harmony;
+using static Logger;
 
 // ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable MemberCanBePrivate.Global
@@ -10,32 +14,78 @@ public class RelationTracker
     // we want to know how any given faction feels about another one
     // each faction will have X opinions
     // we want to be able to lookup a faction and get all its opinions back
-    //public SimGameState sim = UnityGameInstance.BattleTechGame.Simulation;
-    public List<Dictionary<Faction, int>> Factions = new List<Dictionary<Faction, int>>();
+    public List<FactionTracker> Factions = new List<FactionTracker>();
 
-    public RelationTracker()
+    public RelationTracker(SimGameState sim)
     {
         // we instantiate the tracker with all the factions, adding them to the list
-        foreach (var faction in UnityGameInstance.BattleTechGame.Simulation.FactionsDict.Keys)
+        // set values based on enemy/neutral/ally FactionDef property
+
+        foreach (var kvp in sim.FactionsDict)
         {
-            Factions.Add(new Dictionary<Faction, int>(RelationshipValues));
+            if (Core.Settings.ExcludedFactions.Contains(kvp.Key)) continue;
+            if (kvp.Value != null)
+                Factions.Add(new FactionTracker(kvp.Key));
+        }
+
+        Log("FACTIONS");
+        foreach (var faction in Factions)
+        {
+            foreach (var kvp in faction.killList)
+            {
+                Log($"{kvp.Key}, {kvp.Value}");
+            }
         }
     }
 
-    public Dictionary<Faction, int> RelationshipValues = new Dictionary<Faction, int>
+    public class FactionTracker
     {
-        {Faction.Liao, 0},
-        {Faction.Steiner, 0},
-        {Faction.Marik, 0},
-        {Faction.Davion, 0},
-        {Faction.Kurita, 0},
-        {Faction.AuriganDirectorate, 0},
-        {Faction.TaurianConcordat, 0},
-        {Faction.MagistracyOfCanopus, 0},
-        {Faction.NoFaction, 0},
-        {Faction.Locals, 0},
-        {Faction.AuriganRestoration, 0}
-    };
+        private SimGameState sim = UnityGameInstance.BattleTechGame.Simulation;
+        public Faction faction;
+        private FactionDef factionDef;
+        public Dictionary<Faction, int> killList = new Dictionary<Faction, int>();
+
+        public FactionTracker(Faction faction)
+        {
+            this.faction = faction;
+            factionDef = sim.FactionsDict
+                .Where(kvp => kvp.Key == faction)
+                .Select(kvp => kvp.Value).First();
+
+            // KillList init
+            foreach (var factionDef in sim.FactionsDict.Values)
+            {
+                if (Core.Settings.ExcludedFactions.Contains(factionDef.Faction)) continue;
+                if (factionDef.Enemies.Contains(factionDef.Faction))
+                {
+                    killList.Add(factionDef.Faction, 75);
+                }
+                else if (factionDef.Allies.Contains(factionDef.Faction))
+                {
+                    killList.Add(factionDef.Faction, 25);
+                }
+                else
+                {
+                    killList.Add(factionDef.Faction, 50);
+                }
+            }
+        }
+    }
+
+    //public Dictionary<Faction, int> KillList = new Dictionary<Faction, int>
+    //{
+    //    {Faction.Liao, 0},
+    //    {Faction.Steiner, 0},
+    //    {Faction.Marik, 0},
+    //    {Faction.Davion, 0},
+    //    {Faction.Kurita, 0},
+    //    {Faction.AuriganDirectorate, 0},
+    //    {Faction.TaurianConcordat, 0},
+    //    {Faction.MagistracyOfCanopus, 0},
+    //    {Faction.NoFaction, 0},
+    //    {Faction.Locals, 0},
+    //    {Faction.AuriganRestoration, 0}
+    //};
 }
 
 //public Dictionary<Faction, Dictionary<Faction, int>> RelationshipAttitudes = new Dictionary<Faction, Dictionary<Faction, int>>
