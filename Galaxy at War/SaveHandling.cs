89b3c8b8cdd1583a8622fb692e6f2311b7d11053
gnivler <gnivil@gmail.com>
2019-05-27 22:12:49 -0600

    add missing file

--- Galaxy at War/SaveHandling.cs
+++ Galaxy at War/SaveHandling.cs
@@ -39,6 +39,9 @@ public class SaveHandling
             SerializeWar();
         }
     }
+    
+    // TODO when quitting - clear 
+    //[HarmonyPatch(
 
     internal static void SerializeWar()
     {
