826f24c9da497e2b80eeea82b82db4e4ae619467
gnivler <gnivil@gmail.com>
2019-05-27 18:20:02 -0600

    fix loading not restoring war

--- Galaxy at War/SaveHandling.cs
+++ Galaxy at War/SaveHandling.cs
@@ -1,6 +1,8 @@
 using System.IO;
 using BattleTech;
 using BattleTech.Save;
+using BattleTech.Save.Core;
+using BattleTech.Save.Test;
 using Harmony;
 using Newtonsoft.Json;
 
@@ -12,16 +14,19 @@ public class SaveHandling
         public static void Postfix()
         {
             if (UnityGameInstance.BattleTechGame.Simulation == null) return;
+            Logger.Log("PostDeserialization Postfix");
             DeserializeWar();
+            
         }
     }
 
-    [HarmonyPatch(typeof(GameInstanceSave), "PreSerialization")]
-    public class GameInstanceSave_PreSerialization_Patch
+    [HarmonyPatch(typeof(SerializableReferenceContainer), "Save")]
+    public class SerializableReferenceContainer_Save_Patch
     {
         public static void Postfix()
         {
             if (UnityGameInstance.BattleTechGame.Simulation == null) return;
+            Logger.Log("Save Postfix");
             SerializeWar();
         }
     }
@@ -31,6 +36,7 @@ public class SaveHandling
         var fileName = $"WarStatus_{UnityGameInstance.BattleTechGame.Simulation.InstanceGUID}.json";
         using (var writer = new StreamWriter("Mods\\GalaxyAtWar\\" + fileName))
             writer.Write(JsonConvert.SerializeObject(Core.WarStatus));
+        Logger.Log(">>> Serialization complete");
     }
 
     internal static void DeserializeWar()
@@ -38,5 +44,6 @@ public class SaveHandling
         var fileName = $"WarStatus_{UnityGameInstance.BattleTechGame.Simulation.InstanceGUID}.json";
         using (var reader = new StreamReader("Mods\\GalaxyAtWar\\" + fileName))
             Core.WarStatus = JsonConvert.DeserializeObject<WarStatus>(reader.ReadToEnd());
+        Logger.Log(">>> Deserialization complete");
     }
 }
