98be2d1fad3cd7a40931190398ebf1c6fa8859cb
gnivler <gnivil@gmail.com>
2019-05-29 12:33:14 -0600

    WIP

--- Galaxy at War/SaveHandling.cs
+++ Galaxy at War/SaveHandling.cs
@@ -13,15 +13,16 @@ using UnityEngine;
 public static class SaveHandling
 {
     private static readonly SimGameState sim = UnityGameInstance.BattleTechGame.Simulation;
-
+    private const string tagPrefix = "GalaxyAtWar{";
+    
     [HarmonyPatch(typeof(SimGameState), nameof(SimGameState.Rehydrate))]
     public static class SimGameState_Rehydrate_Patch
     {
         public static void Postfix()
         {
-            Logger.Log("Rehydrate Postfix");
             if (UnityGameInstance.BattleTechGame.Simulation == null) return;
-            if (!sim.CompanyTags.Any(x => x.StartsWith("GalaxyAtWarSave"))) return;
+            if (!sim.CompanyTags.Any(x => x.StartsWith(tagPrefix))) return;
+            Logger.Log("Rehydrate Postfix");
             Core.WarStatus = DeserializeWar();
         }
     }
@@ -42,15 +43,43 @@ public static class SaveHandling
     {
         public static void Postfix(SimGameState __instance)
         {
+            // clear the war state completely
+            var hotkeyF10 = (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKeyDown(KeyCode.F10);
+            if (hotkeyF10)
+            {
+                foreach (var tag in sim.CompanyTags)
+                {
+                    if (tag.StartsWith("GalaxyAtWar"))
+                    {
+                        sim.CompanyTags.Remove(tag);
+                        Logger.Log("removed tag");
+                    }
+                    else
+                    {
+                        Logger.Log("left " + tag);
+                    }
+                }
+
+                Core.WarStatus = null;
+            }
+
+            //sim.CompanyTags.Where(tag => tag.StartsWith("GalaxyAtWar")).Do(x => sim.CompanyTags.Remove(x));
+
             var hotkeyD = (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKeyDown(KeyCode.D);
             if (hotkeyD)
                 using (var writer = new StreamWriter("Mods\\GalaxyAtWar\\Dump.json"))
                     writer.Write(JsonConvert.SerializeObject(Core.WarStatus));
 
+            var hotkeyL = (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKeyDown(KeyCode.L);
+            if (hotkeyL)
+            {
+                sim.CompanyTags.Where(x => x.Contains("GalaxyAtWar")).Do(Logger.LogDebug);
+            }
+
             var hotkeyT = (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKeyDown(KeyCode.T);
             if (hotkeyT)
             {
-                var tagLength = sim.CompanyTags.FirstOrDefault(x => x.StartsWith("GalaxyAtWarSave"))?.Length;
+                var tagLength = sim.CompanyTags.FirstOrDefault(x => x.StartsWith(tagPrefix))?.Length;
 
                 global::Logger.Log($"GalaxyAtWarSize {tagLength / 1024}kb");
                 //sim.CompanyTags.Do(Logger.Log);
@@ -63,12 +92,13 @@ public static class SaveHandling
         sim.CompanyTags.Where(tag => tag.Contains(@"{""systems"":[],""relationTracker"":")).Do(x => sim.CompanyTags.Remove(x));
         sim.CompanyTags.Where(tag => tag.StartsWith("GalaxyAtWar")).Do(x => sim.CompanyTags.Remove(x));
         sim.CompanyTags.Add("GalaxyAtWarSave" + JsonConvert.SerializeObject(Core.WarStatus));
-        Logger.Log(">>> Serialization complete");
+        Logger.LogDebug(">>> Serialization complete");
     }
 
     internal static WarStatus DeserializeWar()
     {
-        Logger.Log(">>> Deserialization");
-        return JsonConvert.DeserializeObject<WarStatus>(sim.CompanyTags.First(x => x.StartsWith("GalaxyAtWarSave")).Substring(15));
+        Logger.LogDebug(">>> Deserialization");
+        int length = tagPrefix.Length;
+        return JsonConvert.DeserializeObject<WarStatus>(sim.CompanyTags.First(x => x.StartsWith(tagPrefix)).Substring(length - 1));
     }
 }
