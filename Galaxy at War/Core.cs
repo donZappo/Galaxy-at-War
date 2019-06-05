7406ad51fc9f798ea36c7cfac5f4186672b097ae
Bradley Burcar <btburcar@gmail.com>
2019-06-05 00:53:38 -0400

    Math tweaking.

--- Galaxy at War/Core.cs
+++ Galaxy at War/Core.cs
@@ -80,17 +80,28 @@ public static class Core
     {
         public static void Prefix(SimGameState __instance, int timeLapse)
         {
-            SimGameState sim = UnityGameInstance.BattleTechGame.Simulation;
-
-            LogDebug(">>> PROC");
-            WarTick();
-            SaveHandling.SerializeWar();
-            LogDebug(">>> DONE PROC");
+            //SimGameState sim = UnityGameInstance.BattleTechGame.Simulation;
+           
+            //LogDebug(">>> PROC");
+            //WarTick();
+            //SaveHandling.SerializeWar();
+            //LogDebug(">>> DONE PROC");
         }
 
         public static void Postfix()
         {
             var sim = UnityGameInstance.BattleTechGame.Simulation;
+
+            if (sim.DayRemainingInQuarter%Settings.WarFrequency == 0 )
+            {
+                Log(sim.DayRemainingInQuarter.ToString());
+                LogDebug(">>> PROC");
+                WarTick();
+                SaveHandling.SerializeWar();
+                LogDebug(">>> DONE PROC");
+            }
+                
+
             //Comstar report on ongoing war.
             if (sim.DayRemainingInQuarter == 30)
             {
