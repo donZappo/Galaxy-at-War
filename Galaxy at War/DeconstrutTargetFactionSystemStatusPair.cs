9a9bddb0e260c06e947c80bdebf533284b085a52
Ross Carran <carran.ross@gmail.com>
2020-11-24 13:48:05 +1300

    Added a Deconstrutor class for the Decontructor in Resources

---
+++ Galaxy at War/DeconstrutTargetFactionSystemStatusPair.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyatWar
+{
+    public static class DeconstrutTargetFactionSystemStatusPair
+    {
+        public static void Deconstruct(this KeyValuePair<string, SystemStatus> randElement, out string target, out SystemStatus system)
+        {            
+            target = randElement.Key;
+            system = randElement.Value;
+        }
+    }
+}
