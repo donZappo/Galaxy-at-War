65718cebe48265b5a4df536b398e1d018dd07a6f
Ross Carran <carran.ross@gmail.com>
2020-11-25 13:34:47 +1300

    changed Logger so it can be instanciated in other blocks of code to output to another file.

--- Galaxy at War/Logger.cs
+++ Galaxy at War/Logger.cs
@@ -7,12 +7,18 @@ using static GalaxyatWar.Globals;
 
 namespace GalaxyatWar
 {
-    public static class Logger
+    public class Logger
     {
         private static string logFilePath;
+        private static string logFileName = "Galaxy-at-War.log";
+
+        public Logger(string lfName)
+        {
+            logFileName = lfName;
+        }
 
         private static string LogFilePath =>
-            logFilePath ?? (logFilePath = Directory.GetParent(Assembly.GetExecutingAssembly().Location).FullName + "/Galaxy-at-War.log");
+            logFilePath ?? (logFilePath = Directory.GetParent(Assembly.GetExecutingAssembly().Location).FullName + "/" + logFileName);
 
         public static void Error(Exception ex)
         {
