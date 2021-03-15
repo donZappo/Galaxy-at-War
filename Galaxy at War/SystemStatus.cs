76b46612d2f3276669e92187d67977f138ac7b26
Ross Carran <carran.ross@gmail.com>
2021-03-15 15:19:25 +1300

    Added code that actually toggled hasDoneDistrobution

--- Galaxy at War/SystemStatus.cs
+++ Galaxy at War/SystemStatus.cs
@@ -148,6 +148,7 @@ namespace GalaxyatWar
                         }
                     }
                 }
+                this.systemResources.hasDoneDistrobution = true;
             }
         }
 
