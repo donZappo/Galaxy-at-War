ca1a0568e156daa49818c9212344f63814d28e2f
Ross Carran <carran.ross@gmail.com>
2021-03-19 07:53:26 +1300

    Broke up CalculateSystemInfluence into manageable blocks and condensed where possible.

--- Galaxy at War/SystemStatus.cs
+++ Galaxy at War/SystemStatus.cs
@@ -254,6 +254,8 @@ namespace GalaxyatWar
         }
 
         // determine starting influence based on neighboring systems
+        // TODO seperating this block out into external methods that are called internally
+        // Will condense if possible, hopefully will be easier to navigate when finished.
         public void CalculateSystemInfluence()
         {
             influenceTracker.Clear();
@@ -264,121 +266,110 @@ namespace GalaxyatWar
                 if (owner == "Locals")
                     influenceTracker.Add("Locals", 100);
 
-                if (owner != "NoFaction" && owner != "Locals")
-                {
-                    influenceTracker.Add(owner, Globals.Settings.DominantInfluence);
-                    var remainingInfluence = Globals.Settings.MinorInfluencePool;
-
-                    if (!(neighborSystems.Keys.Count == 1 && neighborSystems.Keys.Contains(owner)) && neighborSystems.Keys.Count != 0)
-                    {
-                        while (remainingInfluence > 0)
-                        {
-                            foreach (var faction in neighborSystems.Keys)
-                            {
-                                if (faction != owner)
-                                {
-                                    var influenceDelta = neighborSystems[faction];
-                                    remainingInfluence -= influenceDelta;
-                                    if (Globals.Settings.DefensiveFactions.Contains(faction))
-                                        continue;
-                                    if (influenceTracker.ContainsKey(faction))
-                                        influenceTracker[faction] += influenceDelta;
-                                    else
-                                        influenceTracker.Add(faction, influenceDelta);
-                                }
-                            }
-                        }
-                    }
-                }
-
-                foreach (var faction in Globals.IncludedFactions)
-                {
-                    if (!influenceTracker.Keys.Contains(faction))
-                        influenceTracker.Add(faction, 0);
-                }
-
-                // need percentages from InfluenceTracker data 
-                var totalInfluence = influenceTracker.Values.Sum();
-                var tempDict = new Dictionary<string, float>();
-                foreach (var kvp in influenceTracker)
-                {
-                    tempDict[kvp.Key] = kvp.Value / totalInfluence * 100;
-                }
-
-                influenceTracker = tempDict;
+                //call to original code segment.
+                UnmodedSystemInfluenceCalc();
             }
             else
             {
+                // Is Processed if HyadesRimCompatible is flagged as true
                 if (owner == "NoFaction" && !starSystem.Tags.Contains("planet_region_hyadesrim"))
                     influenceTracker.Add("NoFaction", 100);
                 if (owner == "Locals" && !starSystem.Tags.Contains("planet_region_hyadesrim"))
                     influenceTracker.Add("Locals", 100);
-                if ((owner == "NoFaction" || owner == "Locals") && starSystem.Tags.Contains("planet_region_hyadesrim"))
+
+                // calls original code segments.
+                ModedSystemInfluenceCalc();
+                UnmodedSystemInfluenceCalc();
+            }
+
+            // both conditions processed this code so condenced and added at end
+            // of method.
+            foreach (var faction in Globals.IncludedFactions)
+            {
+                if (!influenceTracker.Keys.Contains(faction))
+                    influenceTracker.Add(faction, 0);
+            }
+
+            // need percentages from InfluenceTracker data 
+            var totalInfluence = influenceTracker.Values.Sum();
+            var tempDict = new Dictionary<string, float>();
+            foreach (var kvp in influenceTracker)
+            {
+                tempDict[kvp.Key] = kvp.Value / totalInfluence * 100;
+            }
+
+            influenceTracker = tempDict;
+        }
+
+        /********************************************************************************************
+         * 
+         *  This Block was originally in CalculateSystemInfluence, have broken the code
+         *  up into segments so it is easier to follow.
+         *  TODO Put in more meaningfull description.
+         * 
+         ****************************************************************************/
+        internal void UnmodedSystemInfluenceCalc()
+        {
+            if (owner != "NoFaction" && owner != "Locals")
+            {
+                influenceTracker.Add(owner, Globals.Settings.DominantInfluence);
+                var remainingInfluence = Globals.Settings.MinorInfluencePool;
+
+                if (!(neighborSystems.Keys.Count == 1 && neighborSystems.Keys.Contains(owner)) && neighborSystems.Keys.Count != 0)
                 {
-                    foreach (var pirateFaction in starSystem.Def.ContractEmployerIDList)
+                    while (remainingInfluence > 0)
                     {
-                        if (Globals.Settings.HyadesNeverControl.Contains(pirateFaction))
-                            continue;
-
-                        if (!influenceTracker.Keys.Contains(pirateFaction))
-                            influenceTracker.Add(pirateFaction, Globals.Settings.MinorInfluencePool);
-                    }
-
-                    foreach (var pirateFaction in starSystem.Def.ContractTargetIDList)
-                    {
-                        if (Globals.Settings.HyadesNeverControl.Contains(pirateFaction))
-                            continue;
-
-                        if (!influenceTracker.Keys.Contains(pirateFaction))
-                            influenceTracker.Add(pirateFaction, Globals.Settings.MinorInfluencePool);
-                    }
-                }
-
-
-                if (owner != "NoFaction" && owner != "Locals")
-                {
-                    influenceTracker.Add(owner, Globals.Settings.DominantInfluence);
-                    var remainingInfluence = Globals.Settings.MinorInfluencePool;
-
-                    if (!(neighborSystems.Keys.Count == 1 && neighborSystems.Keys.Contains(owner)) && neighborSystems.Keys.Count != 0)
-                    {
-                        while (remainingInfluence > 0)
+                        foreach (var faction in neighborSystems.Keys)
                         {
-                            foreach (var faction in neighborSystems.Keys)
+                            if (faction != owner)
                             {
-                                if (faction != owner)
-                                {
-                                    var influenceDelta = neighborSystems[faction];
-                                    remainingInfluence -= influenceDelta;
-                                    if (Globals.Settings.DefensiveFactions.Contains(faction))
-                                        continue;
-                                    if (influenceTracker.ContainsKey(faction))
-                                        influenceTracker[faction] += influenceDelta;
-                                    else
-                                        influenceTracker.Add(faction, influenceDelta);
-                                }
+                                var influenceDelta = neighborSystems[faction];
+                                remainingInfluence -= influenceDelta;
+                                if (Globals.Settings.DefensiveFactions.Contains(faction))
+                                    continue;
+                                if (influenceTracker.ContainsKey(faction))
+                                    influenceTracker[faction] += influenceDelta;
+                                else
+                                    influenceTracker.Add(faction, influenceDelta);
                             }
                         }
                     }
                 }
-
-                foreach (var faction in Globals.IncludedFactions)
-                {
-                    if (!influenceTracker.Keys.Contains(faction))
-                        influenceTracker.Add(faction, 0);
-                }
-
-                // need percentages from InfluenceTracker data 
-                var totalInfluence = influenceTracker.Values.Sum();
-                var tempDict = new Dictionary<string, float>();
-                foreach (var kvp in influenceTracker)
-                {
-                    tempDict[kvp.Key] = kvp.Value / totalInfluence * 100;
-                }
-
-                influenceTracker = tempDict;
             }
         }
+
+        /***************************************************************************
+         * 
+         *  This Block was originally in CalculateSystemInfluence, have broken the code
+         *  up into segments so it is easier to follow.
+         *  TODO Put in more meaningfull description.
+         * 
+         ****************************************************************************/
+        internal void ModedSystemInfluenceCalc()
+        {
+            if ((owner == "NoFaction" || owner == "Locals") && starSystem.Tags.Contains("planet_region_hyadesrim"))
+            {
+                foreach (var pirateFaction in starSystem.Def.ContractEmployerIDList)
+                {
+                    if (Globals.Settings.HyadesNeverControl.Contains(pirateFaction))
+                        continue;
+
+                    if (!influenceTracker.Keys.Contains(pirateFaction))
+                        influenceTracker.Add(pirateFaction, Globals.Settings.MinorInfluencePool);
+                }
+
+                foreach (var pirateFaction in starSystem.Def.ContractTargetIDList)
+                {
+                    if (Globals.Settings.HyadesNeverControl.Contains(pirateFaction))
+                        continue;
+
+                    if (!influenceTracker.Keys.Contains(pirateFaction))
+                        influenceTracker.Add(pirateFaction, Globals.Settings.MinorInfluencePool);
+                }
+            }
+        }
+
+
 
         public void InitializeContracts()
         {
