65ab35ff62a7f589f3766eaa743f671c706234a7
Ross Carran <carran.ross@gmail.com>
2021-03-21 22:33:09 +1300

    Added some notes to the Influence class. Will start to untangle influence from the resource code after going through AllocateDefensiveResources.

--- Galaxy at War/Influence.cs
+++ Galaxy at War/Influence.cs
@@ -6,14 +6,22 @@ using System.Threading.Tasks;
 
 namespace GalaxyatWar
 {
-    //Currently to be wired directly into SystemStatus.
-    //The plan is to have each System manage its own resources and influence.
-    //or as close to that as humanly possible.
-    //In an attempt to clean up the code and make tracking Influence and
-    //resources easier.
-    //Influence will be left untouched and unused untill resource management, is
-    //more stable. Preferably work on influence will not happen untill core resource
-    //code does it's intended job without external influence.
+    // Currently to be wired directly into SystemStatus.
+    // The plan is to have each System manage its own resources and influence.
+    // or as close to that as humanly possible.
+    // In an attempt to clean up the code and make tracking Influence and
+    // resources easier.
+    // Influence will be left untouched and unused untill resource management, is
+    // more stable. Preferably work on influence will not happen untill core resource
+    // code does it's intended job without external influence.
+    //
+    // with how things are currently, this can't realy be done untill resource code and influence code
+    // are untangled from each other.
+    //
+    // After further inspection, will seperate resource and influence from each other
+    // entirely. 
+    // All resource processing will be done first.
+    // Only after that will any influence be processed.
     class Influence
     {
     }
