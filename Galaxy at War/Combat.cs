9db9441953836cf48d7e82a46ff7f7988ea4d2b0
Ross Carran <carran.ross@gmail.com>
2021-03-25 09:49:05 +1300

    More progress with combat. Have commented in what will be used for initial testing, just need to impliment it.

--- Galaxy at War/Combat.cs
+++ Galaxy at War/Combat.cs
@@ -25,15 +25,17 @@ namespace GalaxyatWar
         internal string sysName;
         internal string sysFaction;
         internal WarFaction warFaction;
+        readonly Resource sysResource;
 
         public Combat()
         {
         }
 
-        public Combat(string systemName, string faction, SystemStatus nSystems)
+        public Combat(string systemName, string faction, SystemStatus nSystems, Resource resource)
         {
             sysName = systemName;
             sysFaction = faction;
+            sysResource = resource;
             warFaction = Globals.WarStatusTracker.warFactionTracker.Find(x => x.faction == sysFaction);
         }
 
@@ -94,20 +96,85 @@ namespace GalaxyatWar
          {
             //TODO (Check this) May just move this into FindMostValubleTarget, Havent checked if it is even used outside of it.
             var deathListTracker = warFaction.DeathListTracker;
+            //(n) means Neighbor
+            List<SystemStatus> nSystemsThatCanBeAttacked = new List<SystemStatus>();
 
+            // Determines what neighbor systems can actually be attacked
             foreach (SystemStatus systemStatus in nSystems)
             {
+                // TODO keep an eye on that Globals.WarStatusTracker.HotBox.Contains(systemStatus.name) condition
                 if (warFaction.attackTargets[systemStatus.owner].Contains(systemStatus.name) && !(systemStatus.owner == sysFaction && 
                     Globals.WarStatusTracker.FlashpointSystems.Contains(systemStatus.name) && Globals.WarStatusTracker.HotBox.Contains(systemStatus.name)))
                 {
                     FindMostValubleTarget(systemStatus, deathListTracker);
-
-                    //Actual resource code starts here, think this may all need a few go overs at some point
-                    //Going to make another method to put inside here that does the math for combat.
-                    //TODO Starting point for tomorrow. Thought I'd be a bit further along then this today, but it is what it is.
-                    
+                    nSystemsThatCanBeAttacked.Add(systemStatus);
                 }
             }
+
+            int numSystemsToAtk = nSystemsThatCanBeAttacked.Count;
+            float aRToHitEachNSystemWith = (sysResource.AttackResources - sysResource.BaseSystemAttackResources) / numSystemsToAtk;
+
+            if (aRToHitEachNSystemWith > 0)
+            {
+                foreach (SystemStatus system in nSystemsThatCanBeAttacked)
+                {
+                    SpendAttackResources(system, nSystemsThatCanBeAttacked.Count);
+                }
+            }
+        }
+
+        /*
+         * wasn't sure how resource information should be passed through
+         * so currently as a read only field.
+         * 
+         * There are people who would change it, but if you do beware, it may cause
+         * you problems if your no good at tracking issues (know I definantly do).
+         * May just end up changing it purely because of the already stated reason.
+         * But for now, it is what it is.
+         * 
+         * Thinking may just let systems do a full swing (everything they have atk
+         * wise) above their base attack.
+         * 
+         * Just to see what happens, im assumeing a lot of systems are going to have no defence afterwards.
+         * Going to try a different approach to others being when a system attacks another
+         * The attacking system loses attack resources and the defending system loses defence reources.
+         * 
+         * atk vs atk could happen but def vs def makes no sense to me. atk vs def would be the most consistant.
+         * best case scenario if the system swinging hits for more then the enemies def, check if the enemy has attack
+         * and reduce that value first. or just do a check if TAr == 0 then go ham on the TDr
+         * 
+         * Was thinking of having an overflow, havent thought of how to impliment it though.
+         * But the idea is, when the enemy system has no resources if someone attacks it they get an overflow value 
+         * for their faction towards that system and that may get added 
+         * to the influence calculations in the influence class, when influence is processed.
+         * 
+         * ^^ may not happen it is just an idea, currently just wiring everything to work.
+         *         
+         */        
+        internal void SpendAttackResources(SystemStatus system, float aRToAttackNeighborWith)
+        {
+            float excess = system.systemResources.AttackResources - aRToAttackNeighborWith;
+            if (excess >= 0f)
+            {
+                 //remove attack from neighbor systems attack.
+            }
+            else if (excess < 0 && system.systemResources.DefenceResources >= Math.Abs(excess))
+            {
+                // set neighbors atk to 0
+                // subtract excess from neighbors defence resources
+            }
+            else
+            {
+                // set both neighbor atk and def values to zero
+                // pretty sure im missing something here, need to have a break for a bit.
+
+                //----------------Potential--------------influence-----idea-------will-not-be-coded-here----
+                // potential excess to mess with not sure if this will be used.
+                // maybe excess from factions gets compaired to each other (if there is more then one faction with excess)
+                // the faction that has excess remaining gets an influence boost (remaining opposing factions in system duke it out)
+                //-------------end------potential-----influence-----idea-------------------------------------
+            }
+            //remove attack from self and selfs total resources
         }
 
         //not even sure if this will end up being needed
